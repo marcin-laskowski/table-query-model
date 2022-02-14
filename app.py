8b81ede897731143a518f197847a82b1c519ca8f
Marcin Laskowski <33329142+marcin-laskowski@users.noreply.github.com>
2022-02-14 15:30:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def main():
                          article=article,
                          allow_flagging='never')
     # Use this config when running on Docker
-    iface.launch(server_name="0.0.0.0", server_port=7000)
+    iface.launch(server_name="0.0.0.0", server_port=8000)
     # iface.launch(enable_queue=True)
 
 
